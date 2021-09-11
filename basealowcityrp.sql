ff4cdf5e826b9ea33b687edeca4e140068e57c33
Uzi250 <87033897+Uzi250@users.noreply.github.com>
2021-09-11 17:57:48 +0200

    Add files via upload

---
+++ basealowcityrp.sql
@@ -0,0 +1,690 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : sam. 11 sep. 2021 à 12:43
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `basealowcityrp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `addon_account`
+--
+
+CREATE TABLE `addon_account` (
+  `name` varchar(60) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `shared` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `addon_account_data`
+--
+
+CREATE TABLE `addon_account_data` (
+  `id` int(11) NOT NULL,
+  `account_name` varchar(100) DEFAULT NULL,
+  `money` int(11) NOT NULL,
+  `owner` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `addon_inventory`
+--
+
+CREATE TABLE `addon_inventory` (
+  `name` varchar(60) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `shared` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `addon_inventory_items`
+--
+
+CREATE TABLE `addon_inventory_items` (
+  `id` int(11) NOT NULL,
+  `inventory_name` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `count` int(11) NOT NULL,
+  `owner` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `billing`
+--
+
+CREATE TABLE `billing` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(40) NOT NULL,
+  `sender` varchar(40) NOT NULL,
+  `target_type` varchar(50) NOT NULL,
+  `target` varchar(40) NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `characters`
+--
+
+CREATE TABLE `characters` (
+  `identifier` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `dateofbirth` varchar(255) NOT NULL,
+  `sex` varchar(1) NOT NULL DEFAULT 'f',
+  `height` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `datastore`
+--
+
+CREATE TABLE `datastore` (
+  `name` varchar(60) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `shared` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `datastore_data`
+--
+
+CREATE TABLE `datastore_data` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `owner` varchar(40) DEFAULT NULL,
+  `data` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `items`
+--
+
+CREATE TABLE `items` (
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) NOT NULL,
+  `limit` int(11) NOT NULL DEFAULT -1,
+  `rare` int(11) NOT NULL DEFAULT 0,
+  `can_remove` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `items`
+--
+
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('bread', 'Pain', 20, 0, 1),
+('phone', 'Phone', 1, 0, 1),
+('water', 'Eau', 20, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) DEFAULT NULL,
+  `whitelisted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `jobs`
+--
+
+INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
+('unemployed', 'Chomeur', 0),
+('unemployed2', 'Etat', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `job_grades`
+--
+
+CREATE TABLE `job_grades` (
+  `id` int(11) NOT NULL,
+  `job_name` varchar(50) DEFAULT NULL,
+  `grade` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) NOT NULL,
+  `salary` int(11) NOT NULL,
+  `skin_male` longtext NOT NULL,
+  `skin_female` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `job_grades`
+--
+
+INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+(1, 'unemployed', 0, 'unemployed', 'Chomeur', 100, '{}', '{}'),
+(2, 'unemployed2', 0, 'rsa', 'Etat', 50, '{}', '{}');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `licenses`
+--
+
+CREATE TABLE `licenses` (
+  `type` varchar(60) NOT NULL,
+  `label` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `phone_app_chat`
+--
+
+CREATE TABLE `phone_app_chat` (
+  `id` int(11) NOT NULL,
+  `channel` varchar(20) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `phone_calls`
+--
+
+CREATE TABLE `phone_calls` (
+  `id` int(11) NOT NULL,
+  `owner` varchar(10) NOT NULL COMMENT 'Num tel proprio',
+  `num` varchar(10) NOT NULL COMMENT 'Num reférence du contact',
+  `incoming` int(11) NOT NULL COMMENT 'Défini si on est à l''origine de l''appels',
+  `time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `accepts` int(11) NOT NULL COMMENT 'Appels accepter ou pas'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `phone_messages`
+--
+
+CREATE TABLE `phone_messages` (
+  `id` int(11) NOT NULL,
+  `transmitter` varchar(10) NOT NULL,
+  `receiver` varchar(10) NOT NULL,
+  `message` varchar(255) NOT NULL DEFAULT '0',
+  `time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `isRead` int(11) NOT NULL DEFAULT 0,
+  `owner` int(11) NOT NULL DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `phone_users_contacts`
+--
+
+CREATE TABLE `phone_users_contacts` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(60) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `number` varchar(10) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `display` varchar(64) CHARACTER SET utf8mb4 NOT NULL DEFAULT '-1'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `society_moneywash`
+--
+
+CREATE TABLE `society_moneywash` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(60) NOT NULL,
+  `society` varchar(60) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `twitter_accounts`
+--
+
+CREATE TABLE `twitter_accounts` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8 NOT NULL DEFAULT '0',
+  `password` varchar(50) COLLATE utf8mb4_bin NOT NULL DEFAULT '0',
+  `avatar_url` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `twitter_likes`
+--
+
+CREATE TABLE `twitter_likes` (
+  `id` int(11) NOT NULL,
+  `authorId` int(11) DEFAULT NULL,
+  `tweetId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `twitter_tweets`
+--
+
+CREATE TABLE `twitter_tweets` (
+  `id` int(11) NOT NULL,
+  `authorId` int(11) NOT NULL,
+  `realUser` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `message` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `likes` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `identifier` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `license` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `money` int(11) DEFAULT NULL,
+  `name` varchar(50) COLLATE utf8mb4_bin DEFAULT '',
+  `skin` longtext COLLATE utf8mb4_bin DEFAULT NULL,
+  `job` varchar(50) COLLATE utf8mb4_bin DEFAULT 'unemployed',
+  `job_grade` int(11) DEFAULT 0,
+  `job2` varchar(255) COLLATE utf8mb4_bin DEFAULT 'unemployed2',
+  `job2_grade` int(11) DEFAULT 0,
+  `loadout` longtext COLLATE utf8mb4_bin DEFAULT NULL,
+  `position` varchar(36) COLLATE utf8mb4_bin DEFAULT NULL,
+  `bank` int(11) DEFAULT NULL,
+  `permission_level` int(11) DEFAULT NULL,
+  `group` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `firstname` varchar(50) COLLATE utf8mb4_bin DEFAULT '',
+  `lastname` varchar(50) COLLATE utf8mb4_bin DEFAULT '',
+  `dateofbirth` varchar(25) COLLATE utf8mb4_bin DEFAULT '',
+  `sex` varchar(10) COLLATE utf8mb4_bin DEFAULT '',
+  `height` varchar(5) COLLATE utf8mb4_bin DEFAULT '',
+  `status` longtext COLLATE utf8mb4_bin DEFAULT NULL,
+  `phone_number` varchar(10) COLLATE utf8mb4_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`identifier`, `license`, `money`, `name`, `skin`, `job`, `job_grade`, `job2`, `job2_grade`, `loadout`, `position`, `bank`, `permission_level`, `group`, `firstname`, `lastname`, `dateofbirth`, `sex`, `height`, `status`, `phone_number`) VALUES
+('steam:110000142b3e9e7', 'license:a8eaaa171adc48dc204ad632f84f146b14883136', 10000, '[M]John Clayton????', '{\"chest_1\":0,\"hair_color_1\":0,\"makeup_4\":0,\"age_2\":0,\"glasses_1\":0,\"makeup_2\":0,\"helmet_2\":0,\"tshirt_1\":0,\"torso_1\":0,\"pants_2\":0,\"lipstick_2\":0,\"blemishes_1\":0,\"shoes_1\":0,\"beard_4\":0,\"sex\":0,\"bracelets_1\":-1,\"torso_2\":0,\"beard_2\":0,\"bags_2\":0,\"eye_color\":0,\"eyebrows_4\":0,\"complexion_2\":0,\"hair_2\":0,\"ears_2\":0,\"makeup_3\":0,\"bproof_1\":0,\"blush_2\":0,\"moles_1\":0,\"beard_3\":0,\"eyebrows_2\":0,\"decals_1\":0,\"chain_1\":0,\"moles_2\":0,\"arms\":0,\"beard_1\":0,\"blemishes_2\":0,\"chest_3\":0,\"watches_2\":0,\"sun_2\":0,\"watches_1\":-1,\"sun_1\":0,\"bproof_2\":0,\"eyebrows_3\":0,\"glasses_2\":0,\"mask_1\":0,\"age_1\":0,\"helmet_1\":-1,\"makeup_1\":0,\"hair_1\":0,\"skin\":0,\"ears_1\":-1,\"chain_2\":0,\"blush_3\":0,\"bodyb_1\":0,\"pants_1\":0,\"tshirt_2\":0,\"lipstick_1\":0,\"bags_1\":0,\"lipstick_3\":0,\"decals_2\":0,\"arms_2\":0,\"bodyb_2\":0,\"chest_2\":0,\"lipstick_4\":0,\"blush_1\":0,\"hair_color_2\":0,\"shoes_2\":0,\"bracelets_2\":0,\"face\":0,\"complexion_1\":0,\"mask_2\":0,\"eyebrows_1\":0}', 'unemployed', 0, 'unemployed2', 0, '[]', '{\"z\":35.9,\"y\":-231.5,\"x\":-517.7}', 40000, 0, 'superadmin', 'Yellow', 'Snow', '', 'm', '180', '[{\"val\":100300,\"percent\":10.03,\"name\":\"hunger\"},{\"val\":325225,\"percent\":32.5225,\"name\":\"thirst\"},{\"val\":0,\"percent\":0.0,\"name\":\"drunk\"}]', '610-9742');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_accounts`
+--
+
+CREATE TABLE `user_accounts` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(22) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `money` double NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `user_accounts`
+--
+
+INSERT INTO `user_accounts` (`id`, `identifier`, `name`, `money`) VALUES
+(3, 'steam:110000142b3e9e7', 'black_money', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_inventory`
+--
+
+CREATE TABLE `user_inventory` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(22) NOT NULL,
+  `item` varchar(50) NOT NULL,
+  `count` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `user_inventory`
+--
+
+INSERT INTO `user_inventory` (`id`, `identifier`, `item`, `count`) VALUES
+(5, 'steam:110000142b3e9e7', 'water', 0),
+(6, 'steam:110000142b3e9e7', 'bread', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_licenses`
+--
+
+CREATE TABLE `user_licenses` (
+  `id` int(11) NOT NULL,
+  `type` varchar(60) NOT NULL,
+  `owner` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `addon_account`
+--
+ALTER TABLE `addon_account`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `addon_account_data`
+--
+ALTER TABLE `addon_account_data`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `index_addon_account_data_account_name_owner` (`account_name`,`owner`),
+  ADD KEY `index_addon_account_data_account_name` (`account_name`);
+
+--
+-- Index pour la table `addon_inventory`
+--
+ALTER TABLE `addon_inventory`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `addon_inventory_items`
+--
+ALTER TABLE `addon_inventory_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `index_addon_inventory_items_inventory_name_name` (`inventory_name`,`name`),
+  ADD KEY `index_addon_inventory_items_inventory_name_name_owner` (`inventory_name`,`name`,`owner`),
+  ADD KEY `index_addon_inventory_inventory_name` (`inventory_name`);
+
+--
+-- Index pour la table `billing`
+--
+ALTER TABLE `billing`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `characters`
+--
+ALTER TABLE `characters`
+  ADD PRIMARY KEY (`identifier`),
+  ADD UNIQUE KEY `identifier` (`identifier`);
+
+--
+-- Index pour la table `datastore`
+--
+ALTER TABLE `datastore`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `datastore_data`
+--
+ALTER TABLE `datastore_data`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `index_datastore_data_name_owner` (`name`,`owner`),
+  ADD KEY `index_datastore_data_name` (`name`);
+
+--
+-- Index pour la table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `job_grades`
+--
+ALTER TABLE `job_grades`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `licenses`
+--
+ALTER TABLE `licenses`
+  ADD PRIMARY KEY (`type`);
+
+--
+-- Index pour la table `phone_app_chat`
+--
+ALTER TABLE `phone_app_chat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `phone_calls`
+--
+ALTER TABLE `phone_calls`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `phone_messages`
+--
+ALTER TABLE `phone_messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `phone_users_contacts`
+--
+ALTER TABLE `phone_users_contacts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `society_moneywash`
+--
+ALTER TABLE `society_moneywash`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `twitter_accounts`
+--
+ALTER TABLE `twitter_accounts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Index pour la table `twitter_likes`
+--
+ALTER TABLE `twitter_likes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_twitter_likes_twitter_accounts` (`authorId`),
+  ADD KEY `FK_twitter_likes_twitter_tweets` (`tweetId`);
+
+--
+-- Index pour la table `twitter_tweets`
+--
+ALTER TABLE `twitter_tweets`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_twitter_tweets_twitter_accounts` (`authorId`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`identifier`);
+
+--
+-- Index pour la table `user_accounts`
+--
+ALTER TABLE `user_accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user_inventory`
+--
+ALTER TABLE `user_inventory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user_licenses`
+--
+ALTER TABLE `user_licenses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `addon_account_data`
+--
+ALTER TABLE `addon_account_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `addon_inventory_items`
+--
+ALTER TABLE `addon_inventory_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `billing`
+--
+ALTER TABLE `billing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `datastore_data`
+--
+ALTER TABLE `datastore_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `job_grades`
+--
+ALTER TABLE `job_grades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `phone_app_chat`
+--
+ALTER TABLE `phone_app_chat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT pour la table `phone_calls`
+--
+ALTER TABLE `phone_calls`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122;
+
+--
+-- AUTO_INCREMENT pour la table `phone_messages`
+--
+ALTER TABLE `phone_messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
+
+--
+-- AUTO_INCREMENT pour la table `phone_users_contacts`
+--
+ALTER TABLE `phone_users_contacts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `society_moneywash`
+--
+ALTER TABLE `society_moneywash`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `twitter_accounts`
+--
+ALTER TABLE `twitter_accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT pour la table `twitter_likes`
+--
+ALTER TABLE `twitter_likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=137;
+
+--
+-- AUTO_INCREMENT pour la table `twitter_tweets`
+--
+ALTER TABLE `twitter_tweets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=170;
+
+--
+-- AUTO_INCREMENT pour la table `user_accounts`
+--
+ALTER TABLE `user_accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `user_inventory`
+--
+ALTER TABLE `user_inventory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `user_licenses`
+--
+ALTER TABLE `user_licenses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `twitter_likes`
+--
+ALTER TABLE `twitter_likes`
+  ADD CONSTRAINT `FK_twitter_likes_twitter_accounts` FOREIGN KEY (`authorId`) REFERENCES `twitter_accounts` (`id`),
+  ADD CONSTRAINT `FK_twitter_likes_twitter_tweets` FOREIGN KEY (`tweetId`) REFERENCES `twitter_tweets` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `twitter_tweets`
+--
+ALTER TABLE `twitter_tweets`
+  ADD CONSTRAINT `FK_twitter_tweets_twitter_accounts` FOREIGN KEY (`authorId`) REFERENCES `twitter_accounts` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
